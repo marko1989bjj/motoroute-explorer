07dda138adfc4f40e77ccdf1545c2574b4b76478
marko1989bjj <marko.aleksic1711@gmail.com>
2026-06-23 15:46:11 +0200

    Fix GitHub Pages routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "@/views/Home.vue";
 import Routes from "@/views/Routes.vue";
 import RouteDetail from "@/views/RouteDetail.vue";
 import Favorites from "@/views/Favorites.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory()(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
