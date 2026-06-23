565000f8cbe8fc30d19ce8683db5ca1b0dc781af
marko1989bjj <marko.aleksic1711@gmail.com>
2026-06-23 15:36:26 +0200

    Add GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,11 @@ import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/motoroute-explorer/",
+
   plugins: [vue(), vueDevTools(), tailwindcss()],
+
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
